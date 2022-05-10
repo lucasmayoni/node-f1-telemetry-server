5fac8759390f9154271a2307640caab9fefc07b0
Lucas Mayoni <lucasmayoni@gmail.com>
2022-05-10 08:49:06 -0300

    Agregado algo de logica

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
-import {Parser} from 'binary-parser';
+//import {Parser} from 'binary-parser';
 const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
+const Parser = require('binary-parser').Parser
 
 const HOST = '0.0.0.0';
 const PORT = 20777;
+
+
+const f1_packet_header = new Parser()
+    .uint16le('m_packet_format')
+    .uint8('m_gameMajorVersion')
+    .uint8('m_gameMinorVersion')
+    .uint8('m_packetVersion')
+    .uint8('m_packetId');
+
+
+const f1_packet = new Parser()
+    .endianness("big")
+    .bit4("version")
+    .array("src", {
+        type: "uint8",
+        length: 4
+        }
+    ).array("dst", {
+        type: "uint8",
+        length: 4
+    });
+
+function parseHeader(message){
+    return f1_packet_header.fromBuffer(message);
+}
 
 server.on('error', (err) => {
     console.log(`server error:\n${err.stack}`);
@@ -11,7 +37,9 @@ server.on('error', (err) => {
 });
 
 server.on('message', (msg, rinfo) => {
-    console.log(`server got: ${parse(msg)} from ${rinfo.address}:${rinfo.port}`);
+	// var message = JSON.parse(msg.toString('utf8'));
+    var packetId = parseHeader()
+    console.log(`server got: ${f1_packet.parse(msg)} from ${rinfo.address}:${rinfo.port}`);
 });
 
 server.on('listening', () => {
